3c41231e57b8a0cf80e6359e940eedfaffe27e9c
manh Le <manhld2004@gmail.com>
2023-04-12 13:11:30 +0700

    Lazer & Reloading update

---
+++ Lazer & Reloading/LTextureclass.h
@@ -0,0 +1,84 @@
+#ifndef LTextureclass_H_
+#define LTextureclass_H_
+
+class LTexture
+{
+	public:
+		//Initializes variables
+		LTexture();
+
+		//Deallocates memory
+		~LTexture();
+
+		//Loads image at specified path
+		bool loadFromFile( std::string path );
+
+		#if defined(SDL_TTF_MAJOR_VERSION)
+		//Creates image from font string
+		bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
+		#endif
+
+		//Deallocates texture
+		void free();
+
+		//Renders texture at given point
+
+		void renderMAP( int x, int y );
+
+		void renderCHAR( int x, int y , double angle, SDL_Point tpt);
+
+		void renderBULLET( int x, int y , double angle, SDL_Point tpt);
+
+		void renderfps( int x, int y );
+
+		//Gets image dimensions
+		int getWidth();
+		int getHeight();
+
+	private:
+		//The actual hardware texture
+		SDL_Texture* mTexture;
+
+		//Image dimensions
+		int mWidth;
+		int mHeight;
+};
+
+#if defined(SDL_TTF_MAJOR_VERSION)
+bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColor )
+{
+	//Get rid of preexisting texture
+	free();
+
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont, textureText.c_str(), textColor );
+	if( textSurface != NULL )
+	{
+		//Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer, textSurface );
+		if( mTexture == NULL )
+		{
+			printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
+
+		//Get rid of old surface
+		SDL_FreeSurface( textSurface );
+	}
+	else
+	{
+		printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
+	}
+
+
+	//Return success
+	return mTexture != NULL;
+}
+#endif
+
+#endif // LTextureclass_H_
---
+++ Lazer & Reloading/bulletclass.h
@@ -0,0 +1,27 @@
+#ifndef bulletclass_H_
+#define bulletclass_H_
+
+class bulletcontrol
+{
+    public:
+        static const int bullet_VEL = 45;
+
+        bulletcontrol();
+
+        void add ( bulletstate x );
+
+        void render( bulletstate bullet );
+
+        void move();
+
+        bool check_collision( bulletstate bt );
+
+        void deletelist();
+
+    private:
+
+        queue< bulletstate > bullet_list;
+        bool bstate;
+};
+
+#endif // bulletclass_H_
